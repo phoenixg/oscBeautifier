8b77238226d0d5a98d2fa1be0824eebb68b496c8
phoenixg <306761352@qq.com>
2014-07-04 17:02:42 +0800

    a

--- background.js
+++ background.js
@@ -1,16 +1,29 @@
 // webRequest api用法参考： http://www.adambarth.com/experimental/crx/docs/webRequest.html
 
+// chrome.webRequest.onBeforeRequest.addListener(
+//     function(){ 
+//         return {cancel: true}; 
+//     },
+//     {
+//         urls: [
+//          "http://my.oschina.net/js/syntax-highlighter-2.1.382/scripts/brush.js",
+//          "http://my.oschina.net/js/syntax-highlighter-2.1.382/styles/shCore.css",
+//             "http://my.oschina.net/js/syntax-highlighter-2.1.382/styles/shThemeDefault.css"
+//         ],
+//         types: ["script","stylesheet"]
+//     },
+//     ["blocking"]
+// );
+
 chrome.webRequest.onBeforeRequest.addListener(
-    function(){ 
-        return {cancel: true}; 
+    function() {
+        return {redirectUrl: chrome.runtime.getURL("brush.js") };
     },
     {
         urls: [
-        	"http://my.oschina.net/js/syntax-highlighter-2.1.382/scripts/brush.js",
-        	"http://my.oschina.net/js/syntax-highlighter-2.1.382/styles/shCore.css",
-            "http://my.oschina.net/js/syntax-highlighter-2.1.382/styles/shThemeDefault.css"
+            "http://my.oschina.net/js/syntax-highlighter-2.1.382/scripts/brush.js"
         ],
-        types: ["script","stylesheet"]
+        types: ["script"]
     },
     ["blocking"]
 );
---
+++ brush.js
@@ -0,0 +1,3 @@
+if (!window.SyntaxHighlighter) window.SyntaxHighlighter = {};
+if (!SyntaxHighlighter.config) SyntaxHighlighter.config = {};
+SyntaxHighlighter.all = function() {};
